ff753dc7a860dbf3e54855a971b752cd4223cc92
hrishi0897-lgtm <hrishi0897@gmail.com>
2026-09-11 08:05:02 +0530

    Initialize pure AOSP emulator target config

---
+++ aosp/target/emulator_aosp_x86_64.mk
@@ -0,0 +1,5 @@
+# Pure AOSP generic x86_64 target configuration
+PRODUCT_NAME := aosp_phone64_x86_64
+PRODUCT_DEVICE := generic_x86_64
+PRODUCT_BRAND := Android
+PRODUCT_MODEL := AOSP on IA Emulator
